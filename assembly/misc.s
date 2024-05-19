4ba4a8c877bcf977ab943ddc7f15172e636e1d11
Nathan <bluroseai@hotmail.com>
2024-05-19 01:20:04 -0400

    money zeros out fix

--- assembly/misc.s
+++ assembly/misc.s
@@ -29,3 +29,34 @@ ldr r0, =0x810353A | 1;
 bx r0
 
 .pool
+
+
+.equ Flag, 0x2322
+
+.align 2
+
+.global DisablePrizeMoney2
+DisablePrizeMoney2:
+    push {r1}
+    ldr r0, =Flag
+    ldr r2, =(0x806E6D0 +1)
+    bl bx_r2
+    pop {r1}
+    cmp r0, #0x1
+    beq skipDisablePrize2
+    add r0, r4, #0x0
+    ldr r2, =(0x809fdd8 +1)
+    bl bx_r2
+
+skipDisablePrize2:
+    ldr r2, =(0x80a0058 +1)
+    bl bx_r2
+    ldr r2, =(0x8054BEA +1)
+    bl bx_r2
+
+.pool
+
+.align 2
+
+bx_r2:
+	bx r2
